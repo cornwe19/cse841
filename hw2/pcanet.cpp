d615dcea3c35825bff4dd53867b0173acb10990e
Cornwell, Dennis <d.cornwell@techsmith.com>
2013-10-09 23:13:25 -0400

    Moving cmd parsing logic into the settings class

--- hw2/pcanet.cpp
+++ hw2/pcanet.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <fstream>
 #include <cmath>
 #include <climits>
@@ -12,50 +11,6 @@
 #define IMAGE_SIZE 64*88
 
 using namespace std;
-
-void usage( char** argv ) {
-   fprintf( stderr, "Usage: %s [-l NUM_EPOCHS] -f FILE_NAME_LIST -d NETWORK_FILE -o OUTPUT_FILE\n", argv[0] );
-   exit( 1 );
-}
-
-void parseCmdArgs( int argc, char** argv, Settings* settings ) {
-   if ( argc % 2 != 1 ) {
-      usage( argv );
-   }
-
-   int i = 1;
-   for ( ; i < argc; i += 2 ) {
-      char* option = argv[i];
-      char* value  = argv[i+1];
-
-      if ( option[0] == '-' ) {
-         switch( option[1] ) {
-            case 'd':
-               settings->networkFile = value;
-               break;
-            case 'f':
-               settings->listingFile = value;
-               break;
-            case 'l':
-               settings->numEpochs = atoi( value );
-               break;
-            case 'o':
-               settings->outputFile = value;
-               break;
-            case 't':
-               settings->testImage = value;
-               break;
-            default:
-               usage( argv );
-               break;
-         }
-      }
-   }
-
-   if ( !settings->areValid() ) {
-      usage( argv );
-   }
-}
 
 void amnesic( unsigned t, float* w1, float* w2 ) {
    float t1 = 20;
@@ -100,7 +55,13 @@ void dumpEigFaces( unsigned gen, char* scratch, vector<int*> data ) {
 int main( int argc, char** argv ) {
    Settings settings;
 
-   parseCmdArgs( argc, argv, &settings );
+   char* error = NULL;
+   settings.load( argc, argv, &error );
+
+   if ( error != NULL ) {
+       fprintf( stderr, "%s", error );
+       exit( 1 );
+   }
 
    Listing listing;
    listing.load( &settings );
--- hw2/settings.h
+++ hw2/settings.h
@@ -1,7 +1,10 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#include <cstddef>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ERROR_MAX 128
 
 class Settings {
 public:
@@ -19,11 +22,57 @@ public:
       testImage   = NULL;
    }
 
+   void load( int argc, char** argv, char** error ) {
+      if ( argc % 2 != 1 ) {
+         err( argv[0], error );
+      }
+
+      int i = 1;
+      for ( ; i < argc; i += 2 ) {
+         char* option = argv[i];
+         char* value  = argv[i+1];
+
+         if ( option[0] == '-' ) {
+            switch( option[1] ) {
+               case 'd':
+                  networkFile = value;
+                  break;
+               case 'f':
+                  listingFile = value;
+                  break;
+               case 'l':
+                  numEpochs = atoi( value );
+                  break;
+               case 'o':
+                  outputFile = value;
+                  break;
+               case 't':
+                  testImage = value;
+                  break;
+               default:
+                  err( argv[0], error );
+                  break;
+            }
+         }
+      }
+
+      if ( !areValid() ) {
+         err( argv[0], error );
+      }
+   }
+
    bool areValid() {
       return listingFile != NULL &&
              networkFile != NULL &&
              outputFile  != NULL;
    }
+
+private:
+   void err( char* progName, char** error ) {
+       *error = new char[ERROR_MAX];
+       sprintf( *error, "Usage: %s [-l NUM_EPOCHS] -f FILE_NAME_LIST -d NETWORK_FILE -o OUTPUT_FILE\n", progName );
+   }
+
 };
 
 #endif
