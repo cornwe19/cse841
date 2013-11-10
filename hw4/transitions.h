9e565a18163081d37a4b08e1bbce1be8fdd44f04
Cornwell, Dennis <d.cornwell@techsmith.com>
2013-11-10 09:40:26 -0500

    Making a static helper class to perform training transitions in Z

--- hw4/transitions.h
+++ hw4/transitions.h
@@ -1,3 +1,9 @@
+/**
+* Dennis Cornwell
+* Class used in traning only to transition the Z area from state to state as a response to
+* new X inputs. Holds a full mapping of the training state transition diagram.
+**/
+
 #ifndef TRANSITIONS_H
 #define TRANSITIONS_H
 
@@ -13,6 +19,35 @@ const unsigned TRANSITIONS[FA_STATES][VOCAB_SIZE] = {
    { 0, 0, 5, 2, 0, 4, 0, 0, 3, 1 },
    { 0, 0, 5, 2, 0, 4, 0, 5, 4, 1 },
    { 0, 0, 5, 2, 0, 4, 0, 0, 5, 1 }
+};
+
+class Transitions {
+public:
+
+   static void updateState( double* Z, double* X, unsigned xSize ) {
+      // Reconstruct vocabulary ID
+      unsigned vocabId = 0;
+      unsigned factor = (unsigned)pow( 2, xSize-1 );
+      for ( unsigned i = 0; i < xSize; i++ ) {
+         vocabId += X[i] * factor;
+         factor /= 2;
+      }
+
+      // Read current state
+      unsigned lastState = 0;
+      for ( unsigned i = 0; i < FA_STATES; i++ ) {
+         if ( Z[i] > 0 ) {
+            lastState = i;
+            break;
+         }
+      }
+
+      // 0 out last state, follow transition, turn on new state
+      Z[lastState] = 0;
+      unsigned nextState = TRANSITIONS[lastState][vocabId-1];
+      Z[nextState] = 1;
+   }
+
 };
 
 #endif
